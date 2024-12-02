d9c425a31b61483ca2c5a479875b683ecde000f7
DavidMonzonGonzalez <dmg11052005@gmail.com>
2024-12-02 09:32:53 +0000

    Validación por JS

--- DEW/UT04-01/script.js
+++ DEW/UT04-01/script.js
@@ -1,3 +1,22 @@
+
+const DOM = {
+    formulario: document.getElementById('formulario'),
+    lista: [
+        nombreUsuario = document.getElementById('userName'),
+        contraseña = document.getElementById('password'),
+        nombre = document.getElementById('Nombre'),
+        apellidos = document.getElementById('Apellidos'),
+        telefono = document.getElementById('telefono'),
+        codigoPostal = document.getElementById('codigoPostal'),
+        dni_nie = document.getElementById('DNI-NIE'),
+        titulo = document.getElementById('titulo'), 
+        descripcion = document.getElementById('Descripcion')
+    ],
+    aficiones: document.getElementsByName("aficiones")
+}
+
+window.onload=generarAnios();
+
 function generarAnios() {
     const selectAnio = document.getElementById("AnioNacimiento");
     const anioActual = new Date().getFullYear(); 
@@ -9,22 +28,16 @@ function generarAnios() {
         selectAnio.appendChild(opcion);
     }
 }
-
-generarAnios();
-
 function conteoTitulo() {
     let conteo = document.getElementById('titulo').value;
     let cambio = document.getElementById('contadorTitulo');
     cambio.innerHTML = `${conteo.length}/15`; 
 }
-
 function conteoDescr(){
     let conteo = document.getElementById('Descripcion').value;
     let cambio = document.getElementById('contadorDescrip');
     cambio.innerHTML = `${conteo.length}/120`; 
 }
-
-
 document.getElementById('showPassword').addEventListener('change', function() {
     const passwordField = document.getElementById('password');
     if (this.checked) {
@@ -33,3 +46,28 @@ document.getElementById('showPassword').addEventListener('change', function() {
         passwordField.type = 'password';
     }
 });
+function conteoChecked(){
+    let conteo = 0;
+    DOM.aficiones.forEach(elemento =>{
+        if(elemento.checked){
+            conteo++;
+        }
+    })
+    return conteo;
+}
+
+DOM.formulario.addEventListener("submit", (e)=>{
+    if(conteoChecked() < 2){
+        e.preventDefault()
+        alert("Debe seleccionar al menos dos aficiones")
+    };
+    let noRellenado = [];
+    DOM.lista.map(input => {
+        if(input.value === ""){
+            e.preventDefault()
+            noRellenado.push(input.name);
+        }
+    });
+    alert(`No se han rellenado los siguientes campos: \n ${noRellenado.join(`\n`)}`);
+})
+
